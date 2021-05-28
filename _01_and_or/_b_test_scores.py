c3904d68fe6a5d43c6646a1213845e6111250fe8
root <root@root.com>
2021-05-28 23:58:59 +0000

    pi aloud ip

--- _01_and_or/_b_test_scores.py
+++ _01_and_or/_b_test_scores.py
@@ -13,6 +13,27 @@ from tkinter import messagebox, simpledialog, Tk
 #   D = 59.5 to less than 69.5
 #   F = less than 59.5
 if __name__ == '__main__':
+
+    window = Tk()
+    window.withdraw()
+
+    test1 = simpledialog.askstring(None, "What did you get on your first test?")
+    test2 = simpledialog.askstring(None, "What did you get on your second test?")
+    int_test1 = float(test1)
+    int_test2 = float(test2)
+
+    both_tests = (int_test1+int_test2)/2
+    both_tests
+    if both_tests >= 89.5:
+        messagebox.showinfo(None, 'You got a ' + str(both_tests) + ' Thats an A, you must have studied hard!')
+    elif both_tests >= 79.5 and both_tests < 89.5:
+        messagebox.showinfo(None, 'You got a ' + str(both_tests) + ' Thats a B, not bad!')
+    elif both_tests >= 69.5 and both_tests < 79.5:
+        messagebox.showinfo(None, 'You got a ' + str(both_tests) + ' Thats a C, you need to study more!')
+    elif both_tests >= 59.5 and both_tests < 69.5:
+        messagebox.showinfo(None, 'You got a ' + str(both_tests) + ' Thats a D, you need to work harder and sleep more!')
+    elif both_tests >= 49.5 and both_tests < 59.5:
+        messagebox.showinfo(None, 'You got a ' + str(both_tests) + ' Thats an F, EEK!')
     # TODO) Ask the user for their score on the FIRST test and store their
     #  score in a variable
 
--- _02_boolean/_b_sleepy_head.py
+++ _02_boolean/_b_sleepy_head.py
@@ -2,9 +2,56 @@
 Use boolean variables to control program logic between two different code
 paths.
 """
+import turtle
+from tkinter import messagebox, Tk
 
+from turtle import Turtle
 
 if __name__ == '__main__':
+
+    window = Tk()
+    window.withdraw()
+    isaac = turtle.Turtle()
+
+    weekend_var1 = True
+    test_passed = True
+    game_over = True
+
+    is_red = True
+    is_square = True
+
+    if weekend_var1:
+        messagebox.showinfo(None, 'it is the weekend!')
+    else:
+        messagebox.showinfo(None, 'So close! It is a weekday :(')
+
+    if test_passed:
+        messagebox.showinfo(None, 'You passed!')
+    else:
+        messagebox.showinfo(None, 'You failed:(')
+
+    if game_over:
+        messagebox.showinfo(None, 'You died, Game Over!')
+    else:
+        messagebox.showinfo(None, 'You are still alive')
+
+    if is_red:
+        if is_square:
+            isaac.pencolor('red')
+            isaac.pendown()
+            isaac.forward(5)
+            isaac.right(90)
+            isaac.forward(5)
+            isaac.right(90)
+            isaac.forward(5)
+            isaac.right(90)
+            isaac.forward(5)
+            isaac.right(90)
+        else:
+            messagebox.showinfo(None, 'It is red but not a square')
+    else:
+        messagebox.showinfo(None, 'It is neither')
+
     # TODO)
     #  1. Use a boolean variable to indicate if it is the weekend.
     #     Display a different message to the user depending on whether it is
